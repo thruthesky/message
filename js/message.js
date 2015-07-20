f0a0af5e3a0208872cddc39bc31b1fc8402b804a
mark <mark@gmail.com>
2015-07-20 02:03:08 -0400

    js fix

--- js/message.js
+++ js/message.js
@@ -11,15 +11,4 @@ $(function(){
         if ( re ) $("form[name='list']").submit();
         else return false;
     });
-	
-	
-		
-	$("body").on( "click","section.message .table-list .row", function( e ){
-		/*
-		*temp
-		*/
-		var $this = $(this);		
-		if( $(e.target).attr("class") == 'ids' ) e.stopPropagation();
-		if( $(e.target).attr("class") == 'checkbox-wrapper' ) e.preventDefault();
-	} );
 });
